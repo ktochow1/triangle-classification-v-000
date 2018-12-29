4acdadadb4c209841f53af36ab6d73fb311dec3a
ktochow1 <ktochow1@binghamton.edu>
2018-12-29 22:08:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ attr_accessor :side1, :side2, :side3
   
   
   def kind
-    if side1 <= 0 || side2 <= 0 || side3 >= 0 
+    if side1 <= 0 || side2 <= 0 || side3 <= 0 
       begin
       raise TriangleError
       end
