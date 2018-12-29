c30e8e81042a72c75ed40d31b386573bbfd03a8d
ktochow1 <ktochow1@binghamton.edu>
2018-12-29 22:05:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,9 @@ attr_accessor :side1, :side2, :side3
   
   
   def kind
-    if side1 == side2 && side2 == side3
+    if side1 == 0 || side2 == 0 || side3 == 0 
+      
+      side1 == side2 && side2 == side3
       :equilateral
     elsif side1 == side2 && side2 > side3
       :isosceles
@@ -20,7 +22,7 @@ attr_accessor :side1, :side2, :side3
       :isosceles
     elsif side1 != side2 && side1 != side2 && side2 != side3
       :scalene
-    elsif side1 <= 0 && side2 <= 0 && side3 <= 0 
+    elsif side1 == 0 && side2 == 0 && side3 == 0 
       begin
       raise TriangleError
       rescue TriangleError => error
