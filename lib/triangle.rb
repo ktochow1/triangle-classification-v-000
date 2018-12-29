cfa09adf17adcf0abe6bacf9341f8fe4541996a2
ktochow1 <ktochow1@binghamton.edu>
2018-12-29 21:55:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ attr_accessor :side1, :side2, :side3
     elsif side1 <= 0 && side2 <= 0 && side3 <= 0 
       begin
       raise TriangleError
-      rescue TriangleError
+      rescue TriangleError 
     end
     end
   end
